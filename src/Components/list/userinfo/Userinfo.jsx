43615a3dedc6f572e7cb7b325bb657894935d0fa
Ragnarloki <sivasankarswaminathan62@gmail.com>
2024-05-20 10:40:46 +0530

    commited

--- src/Components/list/userinfo/Userinfo.jsx
+++ src/Components/list/userinfo/Userinfo.jsx
@@ -6,12 +6,12 @@ function Userinfo() {
         <div className="user">
             <img src="/avatar.png" alt="" />
             <h2>john</h2>
-        </div>
+        
         <div className="icons">
             <img src="./more.png" alt="" />
             <img src="./video.png" alt="" />
              <img src="/edit.png" alt="" />
-        </div>
+        </div></div>
     </div>
   )
 }
